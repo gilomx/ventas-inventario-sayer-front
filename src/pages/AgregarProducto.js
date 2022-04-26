0cf75b08af259dc11f30144756330eca37dd30ab
gilomx <mgt.gilou@gmail.com>
2022-04-26 12:56:39 -0500

    AgregarProducto - Uppercase en código de producto

--- src/pages/AgregarProducto.js
+++ src/pages/AgregarProducto.js
@@ -1,4 +1,21 @@
+import {useState} from 'react'
 export const AgregarProducto = () => {
+
+    const [form, setForm] = useState({
+        productCode:'',
+        purchasePrice:'',
+        salePrice:'',
+        category:'',
+        name:'',
+        description:''
+    })
+
+    const handleChange = ({ target: { name, value }}) => {
+        name === 'productCode' ? setForm({...form, [name]: value.toUpperCase()}) : setForm({...form, [name]: value})
+        console.log('Form:')
+        console.log(form)
+    }
+
     return (
       <>
           <div className='box-content p-2 m-12 rounded-lg bg-white drop-shadow-sm'>
@@ -6,21 +23,41 @@ export const AgregarProducto = () => {
                 <form>
                     <div className="flex flex-auto">
                         <div className="flex-1 p-2">
-                            <label htmlFor="email" className='text-gray-400'>Email</label>
-                            <input type="email" name="email" id="email" 
+                            <label htmlFor="productCode" className='text-gray-400'>Código</label>
+                            <input onChange={handleChange} type="text" name="productCode" id="productCode" 
+                            className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200' 
+                            value={form.productCode}
+                            />
+                            <label htmlFor="purchasePrice" className='text-gray-400'>Precio de compra</label>
+                            <input onChange={handleChange} type="text" name="purchasePrice" id="purchasePrice" 
+                            className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200' 
+                            
+                            />
+                            <label htmlFor="salePrice" className='text-gray-400'>Precio de venta</label>
+                            <input onChange={handleChange} type="text" name="salePrice" id="salePrice" 
+                            className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200' 
+                            
+                            />
+                            <label htmlFor="category" className='text-gray-400'>Categoría</label>
+                            <input onChange={handleChange} type="text" name="category" id="category" 
                             className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200' 
                             
                             />
                         </div>
                         <div className="flex-1 p-2">
-                            <label htmlFor="password" className='text-gray-400'>Password</label>
-                            <input type="password" name="password" id="password"
+                            <label htmlFor="name" className='text-gray-400'>Nombre</label>
+                            <input onChange={handleChange} type="text" name="name" id="name"
                             className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200'
                             
                             />
+                            <label htmlFor="description" className='text-gray-400'>Descripción</label>
+                            <textarea onChange={handleChange} name="description" id="description" rows="4"
+                            className='block min-w-full bg-gray-100 rounded-lg mb-4 p-1.5 focus:outline-none focus:ring-1 focus:ring-gray-200' 
+                            >
+                            </textarea>
                         </div>
                     </div>
-                    <button className='bg-sky-600 text-white px-6 py-1.5 rounded-lg hover:bg-sky-500'>Login</button>
+                    <button className='bg-sky-600 text-white px-6 py-1.5 rounded-lg hover:bg-sky-500'>Agregar</button>
                 </form>
           </div> 
       </>
